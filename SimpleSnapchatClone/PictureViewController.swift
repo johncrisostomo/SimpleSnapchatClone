c3342453339876c6e2d5b2e1553434c9917a0101
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-11 21:17:21 +0800

    prepares picture view controller

---
+++ SimpleSnapchatClone/PictureViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PictureViewController.swift
+//  SimpleSnapchatClone
+//
+//  Created by John Crisostomo on 11/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+        
+    }
+}
