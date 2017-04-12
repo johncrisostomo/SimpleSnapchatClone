59e3e80adca2ab27f0b9197c45fc7752c5a1d0ad
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-12 17:08:30 +0800

    implements snaps view

---
+++ SimpleSnapchatClone/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  SimpleSnapchatClone
+//
+//  Created by John Crisostomo on 12/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var imageDescription = ""
+    var from = ""
+}
--- SimpleSnapchatClone/SnapsViewController.swift
+++ SimpleSnapchatClone/SnapsViewController.swift
@@ -7,16 +7,62 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var snaps: [Snap] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        FIRDatabase.database().reference()
+            .child("users")
+            .child(FIRAuth.auth()!.currentUser!.uid)
+            .child("snaps")
+            .observe(FIRDataEventType.childAdded, with: {
+            (snapshot) in
+                let snap = Snap()
+                
+                snap.imageURL = (snapshot.value as! NSDictionary)["image"] as! String
+                snap.from = (snapshot.value as! NSDictionary)["from"] as! String
+                snap.imageDescription = (snapshot.value as! NSDictionary)["description"] as! String
+                
+                print(snap.from)
+                
+                self.snaps.append(snap)
+                
+                self.tableView.reloadData()
+        })
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
+        do {
+            try FIRAuth.auth()!.signOut()
+        } catch {}
+        
         dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let snap = snaps[indexPath.row]
+        
+        cell.textLabel?.text = snap.from
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
     }
 }
