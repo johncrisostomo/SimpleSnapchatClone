996d2f696627c7995cf00d6fa84f671a519ce44e
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-12 08:45:33 +0800

    prepares select user view and controller

---
+++ SimpleSnapchatClone/SelectUserViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SelectUserViewController.swift
+//  SimpleSnapchatClone
+//
+//  Created by John Crisostomo on 12/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
