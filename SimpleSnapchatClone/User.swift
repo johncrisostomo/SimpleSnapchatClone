3ade8d5605604a733066b4ad03b69019b6765c70
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-12 10:35:02 +0800

    implements user fetching on firebase

---
+++ SimpleSnapchatClone/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SimpleSnapchatClone
+//
+//  Created by John Crisostomo on 12/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
